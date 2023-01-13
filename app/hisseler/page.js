4b565c87ab3b4a8ffaa268b22fd24804334a11ff
aturkkan94 <aturkkan94@gmail.com>
2023-01-13 11:30:31 +0300

    commit3

--- app/hisseler/page.js
+++ app/hisseler/page.js
@@ -1,17 +1,7 @@
 import React from "react";
 
-const getPrice = async (symbol) => {
-  const res = await fetch(
-    `https://query2.finance.yahoo.com/v10/finance/quoteSummary/${symbol.toUpperCase()}.IS?formatted=false&modules=price%2CsummaryDetail%2CfinancialData`
-  );
-  const stock = await res.json();
-  return stock;
-};
-
-async function Home({ params: { symbol } }) {
-  const stock = await getPrice(symbol);
-  console.log(stock);
-  return <div className="page-content">Home</div>;
+async function Home() {
+  return <div className="page-content">Hisseler</div>;
 }
 
 export default Home;
