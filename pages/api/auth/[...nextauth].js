3f5e256460d9b328c85cc329dbc7b870da518a97
aturkkan94 <aturkkan94@gmail.com>
2023-01-20 22:07:47 +0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -25,7 +25,7 @@ export const authOptions = {
       },
       async authorize(credentials, req) {
         const { username, password } = credentials;
-        const res = await fetch("http://127.0.0.1:8000/api/token/", {
+        const res = await fetch(`${process.env.MAIN_API}/token/`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
