c3f09da5be26efc4fa80c9933eda0302d3d62d44
turkkan94 <aturkkan94@outlook.com>
2023-02-19 22:15:42 +0300

    Revert "input fix"
    This reverts commit c0b9d92a424cfef32d64987ed38b6f6a9d3bc786.

--- src/components/common/FavoritesTable.js
+++ src/components/common/FavoritesTable.js
@@ -41,7 +41,7 @@ export default function FavoritesTable({ favorites, user }) {
     <>
       <label className="relative flex px-3">
         <input
-          className="form-input peer h-8 w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 text-[16px] placeholder:text-xs+ ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"
+          className="form-input peer h-8 w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 text-xs+ ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"
           placeholder="Hisse Ara..."
           type="text"
           value={value}
